bce4617ede7b0158dbec132cee65422372b45550
SAHIL KATOCH <80945456+sahil290@users.noreply.github.com>
2021-08-15 18:02:09 +0530

    this is my login page practice

---
+++ login page/login.js
@@ -0,0 +1,10 @@
+document.getElementsByClassName("forgot-password").addEventListener("click", helpText);
+
+function helpText() {
+    alert("Calm Down & Try to remember the Password");
+}
+document.getElementsByClassName("btn-primary").addEventListener("click", alertFunction);
+
+function alertFunction() {
+    alert("Please Wait");
+}
